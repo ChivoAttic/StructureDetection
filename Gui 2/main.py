4d73e0669599d773960546feed749aa66e1e1d9c
Teodoro Hochfarber <teohoch2@gmail.com>
2014-03-27 17:23:52 -0300

    Tratando de mostrar mainwindow desde preview, para botton cancelar

--- Gui 2/main.py
+++ Gui 2/main.py
@@ -1,27 +1,32 @@
 __author__ = 'teohoch'
 
 import sys
-from PyQt4 import QtGui, QtCore
-import pyfits
+from PyQt4.QtGui import *
+from PyQt4.QtCore import *
+#import pyfits
 import os
 
 from intro import Ui_MainWindow as intro
-from preview import *
 from visorpreview import Ui_Preview as visp
 
-class VisorPreview(QtGui.QWidget):
+class VisorPreview(QWidget):
 
     CurrentPath = ""
     CurrentFile = ""
     K = 0
     Nlevel = 0
+    Sinal = pyqtSignal()
 
-    def __init__(self, k , Nlevel, CurrentPath, CurrentFile, parent = None):
-        QtGui.QWidget.__init__(self, parent)
+
+
+    def __init__(self,parent = None):
+        QWidget.__init__(self, parent)
 
         self.ui = visp()
         self.ui.setupUi(self)
+        self.ui.ButtonAceptar.clicked.connect(self.)# TODO seguir aqui!!
 
+    def Iniciar(self, k , Nlevel, CurrentPath, CurrentFile):
         self.CurrentFile = CurrentFile
         self.CurrentPath = CurrentPath
         self.K = k
@@ -30,40 +35,51 @@ class VisorPreview(QtGui.QWidget):
         self.ui.spinBoxK.setValue(k)
         self.ui.spinBoxNiveles.setValue(Nlevel)
 
-    def Cancelar(self):
-        self.hide()
+    def PushAceptar(self):
+        self.emit(self.Sinal)
 
-
-class AppIntro(QtGui.QMainWindow):
-
-
-    Visor = VisorPreview
+class AppIntro(QMainWindow):
 
     def __init__(self, parent=None):
-        QtGui.QWidget.__init__(self, parent)
+        QWidget.__init__(self, parent)
         self.ui = intro()
         self.ui.setupUi(self)
         self.ui.ButtonBuscar.clicked.connect(self.Buscar)
-        self.ui.ButtonCargar.clicked.connect(self.loadFit)
+        self.ui.ButtonCargar.clicked.connect(self.LoadFit)
+
+        self.Visor = VisorPreview()
+
+        self.Visor.Sinal.connect(self.wow)
+
 
     def Buscar(self):
-        path = str(QtGui.QFileDialog.getOpenFileName(self, "Open Fits"))
+        path = str(QFileDialog.getOpenFileName(self, "Open Fits"))
         self.ui.PathLine.setText(path)
 
-    def loadFit(self):
+    def LoadFit(self):
+
         K = self.ui.spinBoxK.value()
         N = self.ui.spinBoxLevel.value()
         Path = str(self.ui.PathLine.text())
         File = os.path.basename(Path)
-        self.Visor = VisorPreview(K,N,Path,File)
+        self.Visor.Iniciar(K,N,Path,File)
         self.Visor.show()
+        self.Visor.ui.ButtonCancelar.clicked.connect(self.CancelarPreview)
         self.hide()
+
+    def CancelarPreview(self):
+        self.Visor.destroy()
+        self.ui.spinBoxK.setValue(0)
+        self.ui.spinBoxLevel.setValue(0)
+        print "k"
+    def wow(self):
+        print("Holop")
 
 
 
 
 if __name__ == "__main__":
-    app = QtGui.QApplication(sys.argv)
+    app = QApplication(sys.argv)
     Aperture = AppIntro()
     Aperture.show()
 
