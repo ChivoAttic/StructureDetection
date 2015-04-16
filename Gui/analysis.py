329ae4c927788c882e0c347925b58217b6f28de6
Mauricio Araya <mauricio@ip157.3.priv.inf.utfsm.cl>
2015-04-16 11:19:29 -0300

    Mods

--- Gui/analysis.py
+++ Gui/analysis.py
@@ -1,6 +1,11 @@
 """
-This file is part of ChiVO
-Copyright (C) Rodrigo Gregorio
+This file is part of ChiVO, the Chilean Virtual Observatory
+A project sponsored by FONDEF (D11I1060)
+Copyright (C) 2015 Universidad Tecnica Federico Santa Maria
+                   Universidad de Chile
+                   Pontificia Universidad Catolica
+                   Universidad de Concepcion
+                   Universidad de Santiago
 
 This program is free software; you can redistribute it and/or
 modify it under the terms of the GNU General Public License
